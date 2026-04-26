6d836513a041356bc20d4d0d0ccaab185cea00d1
Achiel <achiel.dhooghe@ugentracing.be>
2026-04-26 10:21:29 +0200

    Add RS code theory answers to report (section 5.2)
    Covers primitive polynomial proof, generator/check polynomial
    construction, Hamming distance, RS vs BCH, encode/decode
    implementation descriptions, bit error capability, and Pb vs Ps.

--- RSCode.py
+++ RSCode.py
@@ -21,7 +21,22 @@ class RSCode:
         assert np.shape(msg)[1] == self.l, 'the number of columns must be equal to self.l'
         assert type(msg) is galois.GF(2**self.m) , 'each element of msg  must be a galois.GF element'
 
-        #insert your code here
+        GF = galois.GF(2**self.m)
+        nrows = np.shape(msg)[0]
+        npar = self.n - self.k
+        code = GF.Zeros((nrows, self.l + npar))
+
+        for i in range(nrows):
+            # shift msg poly by x^(n-k) then take remainder with g(x)
+            buf = GF.Zeros(self.l + npar)
+            buf[:self.l] = msg[i]
+            rem = galois.Poly(buf) % self.g
+            # galois strips leading zeros so we pad back
+            rc = rem.coeffs
+            par = GF.Zeros(npar)
+            par[npar - len(rc):] = rc
+            code[i, :self.l] = msg[i]
+            code[i, self.l:] = par
 
         assert np.shape(code)[1] == self.l+self.n-self.k , 'the number of columns must be equal to self.l+self.n-self.k'
         assert type(code) is galois.GF(2**self.m) , 'each element of code  must be a galois.GF element'
@@ -37,7 +52,106 @@ class RSCode:
         assert np.shape(code)[1] == self.l+self.n-self.k , 'the number of columns must be equal to self.l+self.n-self.k'
         assert type(code) is galois.GF(2**self.m) , 'each element of code  must be a galois.GF element'
 
-        #insert your code here
+        GF = galois.GF(2**self.m)
+        N, clen = np.shape(code)
+        tt = 2 * self.t
+        a = GF.primitive_element
+
+        decoded = GF.Zeros((N, self.l))
+        nERR = np.zeros(N, dtype=int)
+
+        # need x^2t for modding later (forney)
+        xt_c = GF.Zeros(tt + 1)
+        xt_c[0] = GF(1)
+        xt = galois.Poly(xt_c)
+
+        for row in range(N):
+            w = code[row]
+            rpoly = galois.Poly(w)
+
+            # syndromes
+            syn = GF.Zeros(tt)
+            for j in range(tt):
+                syn[j] = rpoly(a**(self.m0 + j))
+
+            if np.all(syn == 0):
+                decoded[row] = w[:self.l]
+                nERR[row] = 0
+                continue
+
+            # PGZ algorithm - try from max errors down
+            lam = None
+            nerr = 0
+            for v in range(self.t, 0, -1):
+                # syndrome matrix
+                mat = GF.Zeros((v, v))
+                for i in range(v):
+                    for j in range(v):
+                        mat[i, j] = syn[i + j]
+                rhs = GF.Zeros(v)
+                for i in range(v):
+                    rhs[i] = syn[v + i]
+                try:
+                    x = np.linalg.solve(mat, rhs)
+                except np.linalg.LinAlgError:
+                    continue
+                lc = GF.Zeros(v + 1)
+                lc[:v] = x
+                lc[v] = GF(1)  # constant term
+                lam = galois.Poly(lc)
+                nerr = v
+                break
+
+            if lam is None:
+                decoded[row] = w[:self.l]
+                nERR[row] = -1
+                continue
+
+            # chien search
+            errloc = []
+            for i in range(clen):
+                if lam(a**(-i)) == 0:
+                    errloc.append(i)
+
+            if len(errloc) != nerr:
+                decoded[row] = w[:self.l]
+                nERR[row] = -1
+                continue
+
+            # forney for error magnitudes
+            spoly = galois.Poly(syn[::-1])
+            omega = (spoly * lam) % xt
+            lam_d = lam.derivative()
+
+            fix = GF(np.array(w))
+            bad = False
+            for d in errloc:
+                xi = a**(-d)
+                dd = lam_d(xi)
+                if dd == 0:
+                    bad = True
+                    break
+                ev = (a**d)**(1 - self.m0) * omega(xi) / dd
+                fix[clen - 1 - d] = fix[clen - 1 - d] + ev
+
+            if bad:
+                decoded[row] = w[:self.l]
+                nERR[row] = -1
+                continue
+
+            # check if it actually worked
+            cpoly = galois.Poly(fix)
+            valid = True
+            for j in range(tt):
+                if cpoly(a**(self.m0 + j)) != 0:
+                    valid = False
+                    break
+            if valid:
+                decoded[row] = fix[:self.l]
+                nERR[row] = nerr
+            else:
+                decoded[row] = w[:self.l]
+                nERR[row] = -1
 
         assert np.shape(decoded)[1] == self.l, 'the number of columns must be equal to self.l'
         assert type(decoded) is galois.GF(2**self.m) , 'each element of decoded  must be a galois.GF element'
@@ -58,7 +172,14 @@ class RSCode:
         # Output:
         #  -generator: generator polynomial represented by a galois.Poly variable
 
-        #insert your code here
+        GF = galois.GF(2**m)
+        a = GF.primitive_element
+        # multiply (x - a^i) for all roots
+        gen = galois.Poly([1], field=GF)
+        for i in range(m0, m0 + 2*t):
+            root = GF([1, int(a**i)])  # (x + a^i), same as (x - a^i) in char 2
+            gen = gen * galois.Poly(root)
+        generator = gen
 
         assert type(generator) == type(galois.Poly([0],field=galois.GF(2**m))), 'generator must be a galois.Poly object'
         return generator
